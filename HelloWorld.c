ef946429c55587a6630ef0f24dc60be72767634b
YashRana12032007 <ranayash1207@gmail.com>
2025-11-08 20:40:45 +0530

    Add files via upload

---
+++ HelloWorld.c
@@ -0,0 +1,51 @@
+//#include<stdio.h>
+//int main() {
+    //printf("Hello, World!\n");
+   // return 0;
+//}
+
+//#include <stdio.h>
+
+//int main()
+//{
+  //  int n;
+    //printf("Enter a number :- ");
+    //scanf("%d",&n);
+
+    //int a = -1, b = 1, c;
+    //int x;
+    //for(x = 1; x<= n; x++)  {
+        //c = a + b;
+        //printf("%d ",c);
+        //a = b;
+        //b = c;
+    //}
+    
+    //printf("\n");
+    //return 0; 
+//}
+
+
+
+
+
+
+
+int main()
+{
+    int n;
+    printf("Enter a number :- ");
+    scanf("%d",&n);
+
+    isPrime(n) ? printf("Prime Number") : printf("Not Prime Number");
+
+    printf("\n");
+
+    // if(isPrime(n))
+    //     printf("Prime Number");
+    // else
+    //     printf("Not Prime Number");
+    
+    printf("\n");
+    return 0; 
+}
